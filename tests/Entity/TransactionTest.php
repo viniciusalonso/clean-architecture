d25c621e45c551ad95cff7b12b0c2e38e1f08ba0
Vinícius A <vba321@hotmail.com>
2018-05-06 01:12:29 -0300

    Tests for Transaction class

---
+++ tests/Entity/TransactionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class TransactionTest extends TestCase
+{
+    public function testGetValueWhenHasAValueShouldReturnIt()
+    {
+        $transaction = new \Bank\Entity\Transaction(14.09);
+        $this->assertEquals(14.09, $transaction->getValue());
+    }
+}
+
