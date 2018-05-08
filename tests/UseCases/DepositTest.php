652edb390212434d791a00011da2ce6b71a3d7dc
Vinícius A <vba321@hotmail.com>
2018-05-07 22:43:52 -0300

    Improve tests for deposit

--- tests/UseCases/DepositTest.php
+++ tests/UseCases/DepositTest.php
@@ -4,16 +4,27 @@ namespace Tests\UseCases;
 
 use PHPUnit\Framework\TestCase;
 use Bank\UseCases\Deposit;
+use Bank\Entity\Validator;
+use Bank\Entity\Account;
+use Bank\Entity\Transaction;
 
 class DepositTest extends TestCase
 {
     public function testDepositWhenValueIsGreaterThanZeroShouldSumWithAccountCurrentBalance()
     {
-        $validator = new \Bank\Entity\Validator();
-        $account = new \Bank\Entity\Account();
-        $transaction = new \Bank\Entity\Transaction(50);
+        $transaction = $this->prophesize(Transaction::class);
+        $transaction->getValue()->willReturn(50);
 
-        $deposit = new Deposit($account, $transaction, $validator);
+        $validator = $this->prophesize(Validator::class);
+        $validator->isValueGreaterThanZero($transaction->reveal())->shouldBeCalled();
+
+        $account = new Account();
+
+        $deposit = new Deposit(
+            $account,
+            $transaction->reveal(),
+            $validator->reveal()
+        );
         $accountResult = $deposit->deposit();
 
         $this->assertEquals(50, $accountResult->getCurrentBalance());
@@ -26,11 +37,22 @@ class DepositTest extends TestCase
 
     public function testDepositWhenValueIsLessOrEqualZeroShouldThrowAnException()
     {
-        $validator = new \Bank\Entity\Validator();
-        $account = new \Bank\Entity\Account();
-        $transaction = new \Bank\Entity\Transaction(-10);
 
-        $deposit = new Deposit($account, $transaction, $validator);
+        $transaction = $this->prophesize(Transaction::class);
+        $transaction->getValue()->willReturn(-10);
+
+        $validator = $this->prophesize(Validator::class);
+        $validator->isValueGreaterThanZero($transaction->reveal())
+            ->will($this->throwException(new \InvalidArgumentException()));
+
+
+        $account = $this->prophesize(Account::class);
+
+        $deposit = new Deposit(
+            $account->reveal(),
+            $transaction->reveal(),
+            $validator->reveal()
+        );
         $deposit->deposit();
     }
 }
